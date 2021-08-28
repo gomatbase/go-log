1b5f9e06856d04973359fb3fdee72f5ea0feaa1f
GOM <gom@theba.se>
2021-08-28 18:37:32 +0200

    use go-error for errors

--- errors.go
+++ errors.go
@@ -4,21 +4,18 @@
 
 package log
 
-// Simple error type for log errors
-type logError string
-
-func (le logError) Error() string {
-	return string(le)
-}
+import (
+	"github.com/gomatbase/go-error"
+)
 
 const (
 
 	// ErrEmptyLoggerName Error raised when trying to refer to a logger with an empty name
-	ErrEmptyLoggerName = logError("Logger name may not be empty")
+	ErrEmptyLoggerName = err.Error("Logger name may not be empty")
 
 	// ErrReinitializingExistingLogger Error raised when trying to initialize an existing logger with different options
-	ErrReinitializingExistingLogger = logError("trying to initialize an already initialized logger with different options")
+	ErrReinitializingExistingLogger = err.Error("trying to initialize an already initialized logger with different options")
 
 	// ErrLoggerDoesNotExist Error raised when referring to a non-existing logger
-	ErrLoggerDoesNotExist = logError("logger with given name doesn't exist")
+	ErrLoggerDoesNotExist = err.Error("logger with given name doesn't exist")
 )
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gomatbase/go-log
 
 go 1.15
+
+require github.com/gomatbase/go-error v1.1.0
